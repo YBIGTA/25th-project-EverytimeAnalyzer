0b20463f3b059833829d602315821e5f51cde3bd
park_jun_hyoung <gitpdeathwa@yonsei.ac.kr>
2024-08-19 10:15:16 +0900

    [feat] 강의 리스트 초간단 임시 구현

--- fe/src/components/LectureTable.vue
+++ fe/src/components/LectureTable.vue
@@ -1,15 +1,24 @@
 <script setup>
-
+import {sample_lecture_list} from "@/sample_data";
+console.log(sample_lecture_list);
 </script>
 
 <template>
-  <div>
-    <p>강의명</p>
-    <p>교수명</p>
-    <p></p>
+  <div class="container">
+    <div v-for="lecture in sample_lecture_list" :key="lecture.name" class="lectureBoard">
+      <p> {{lecture.name }}</p>
+      <p> {{lecture.code }}</p>
+      <p> {{lecture.professorList }}</p>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.container {
+  display: flex;
+}
 
+.lectureBoard {
+  margin: 10px 20px;
+}
 </style>
--- fe/src/sample_data.js
+++ fe/src/sample_data.js
@@ -1,5 +1,27 @@
-export const sample_lecture_list= [
+export const sample_lecture_list = [
     {
-        
-    }
+        id: 1,
+        code: 'UCE1105-01-00',
+        name: '영화의이해',
+        professorList: ['강철']
+    },
+    {
+        id: 2,
+        code: 'UCE1105-01-00',
+        name: '영화의이해',
+        professorList: ['강철']
+    },
+    {
+        id: 3,
+        code: 'UCE1105-01-00',
+        name: '영화의이해',
+        professorList: ['강철']
+    },
+    {
+        id: 4,
+        code: 'UCE1105-01-00',
+        name: '영화의이해',
+        professorList: ['강철']
+    },
 ]
+//{'_id': ObjectId('66bf38cccac788770e09c22f'), 'code': 'UCE1105-01-00', 'name': '영화의이해', 'place': '광B105/동영상콘텐츠', 'professorList': ['강철'], 'time': {'목': [1], '화': [2, 3]}, 'type': ['대교', '필교', '학필']}
