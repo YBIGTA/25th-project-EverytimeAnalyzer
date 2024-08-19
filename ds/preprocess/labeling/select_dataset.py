86a4ce1bd0123b618a8e61f2f307ec791647f67a
iamzieun <iamhappyhill@yonsei.ac.kr>
2024-08-19 18:09:17 +0900

    [feat] 학습용 데이터 선택을 위한 함수 추가

--- ds/preprocess/labeling/select_dataset.py
+++ ds/preprocess/labeling/select_dataset.py
@@ -12,17 +12,17 @@ from embedding import load_model, generate_embedding
 
 def process_flag_sentences(input_file: str, output_file: str) -> pd.DataFrame:
     """
-    텍스트 파일 읽어와서 전처리 후 embedding vector 생성
+    flag 문장들이 있는 txt 파일 읽어와서 전처리 후 embedding vector 생성
     Args:
-        - input_file: 입력 텍스트 파일 경로
-        - output_file: 출력 CSV 파일 경로
+        - input_file: flag 문장들이 있는 txt 파일 경로
+        - output_file: 출력 csv 파일 경로
     """
     data = []
     
     # 텍스트 파일을 읽어서 처리
     with open(input_file, 'r', encoding='utf-8') as file:
         for line in file:
-            # 각 줄에서 첫 번째 쉼표만 기준으로 분리
+            # 각 줄에서 첫 번째 쉼표만 기준으로 분리 (이후 쉼표는 문장부호로 사용된 쉼표)
             topic, sentence = line.split(',', 1)
             data.append([topic.strip(), sentence.strip()])
 
@@ -38,19 +38,25 @@ def process_flag_sentences(input_file: str, output_file: str) -> pd.DataFrame:
         lambda sentence: generate_embedding(model, sentence)
     )
     
-    # 결과를 CSV 파일로 저장
     df.to_csv(output_file, index=False)
     
     return df
 
-df_result = process_flag_sentences('flag_sentences.txt', 'flag_sentences_embedding.csv')
-df_result
 
 # TODO: 강의평 각 문장들과 flag_sentences의 5개 토픽 * 5개 문장 = 25개 문장 간의 manhattan distance 계산 -> 18000 * 25개의 distance matrix 생성
-def manhattan_distance(v1, v2):
+def manhattan_distance(v1: float, v2: float) -> float:
+    """
+    v1과 v2 사이의 맨하탄 거리 계산
+    """
     return sum(abs(v1 - v2))
 
-def calculate_distance_matrix(review_sentences, flag_sentences):
+def calculate_distance_matrix(review_sentences: pd.DataFrame, flag_sentences: pd.DataFrame) -> pd.DataFrame:
+    """
+    강의평의 각 문장들과 flag 문장들 간의 manhattan distance 계산
+    Args:
+        - review_sentences: 강의평 문장 DataFrame
+        - flag_sentences: flag 문장 DataFrame
+    """
     distance_matrix = []
     for i in range(len(review_sentences)):
         distances = []
@@ -61,6 +67,19 @@ def calculate_distance_matrix(review_sentences, flag_sentences):
 
 
 # TODO: 각 문장들과 토픽별 5개 문장 간의 유사도 중 최소값을 각 문장의 토픽 유사도로 사용 -> 18000 * 5개의 similarity matrix 생성
-
+def select_min_distance(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    강의평의 각 문장들과 topic별 flag 문장들간의 manhattan distance 중 최솟값을 토픽 유사도로 사용
+    """
+    if df.shape[1] != 25:
+        raise ValueError("원본 DataFrame의 컬럼 개수는 25개여야 합니다.")
+    
+    min_distance = []
+    
+    for i in range(0, df.shape[1], 5):
+        group_min = df.iloc[:, i:i+5].min(axis=1)
+        min_distance.append(group_min)
+    
+    return pd.DataFrame(min_distance).transpose()
 
 # TODO: 토픽별로 유사도 상위 250개 문장 추출 후 중복 제거 -> max 1250개 문장과 해당 문장의 소속 토픽 유사도 저장
