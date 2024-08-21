09ea49ae77bf620bdadb1edd1ac07676224dd535
iamzieun <iamhappyhill@yonsei.ac.kr>
2024-08-21 14:42:04 +0900

    [feat] 사용자 입력 받아서 유사 강의평 학정번호 반환

--- be/ChromaRepositoryTest.py
+++ be/ChromaRepositoryTest.py
@@ -1,10 +1,31 @@
+from sentence_transformers import SentenceTransformer
 from ChromaRepository import ChromaRepository
 from env import load_chroma_env_vars
+from embedding import load_model, generate_embedding
 
-env_vars = load_chroma_env_vars()
-repo = ChromaRepository(env_vars["host"], env_vars["port"])
+def get_similar_reviews(query: str, repo: ChromaRepository, model: SentenceTransformer, top_k: int = 4):
+    """
+    사용자에게 입력에 대하여 chromadb에서 유사한 강의평 문장 검색 후 해당 강의평의 학정번호 반환
+    """
+    query_embedding = generate_embedding(model, [query])
+    results = repo.collection.query(query_embeddings=query_embedding, n_results=top_k)
 
+    metadatas = results['metadatas'][0]
+    reviews = results['documents'][0]
+    for metadata, review in zip(metadatas, reviews):
+        print(f"학정번호: {metadata['code']}")
+        print(f"강의평: {review}")
+        print("----------")
+    
+    return [metadata['code'] for metadata in metadatas]
 
-data_path = '/Users/jieunpark/Desktop/25th-project-EverytimeAnalyzer/ds/data/embedding_formatted.json'
-repo.add_data_to_collection(data_path)
-print("데이터 적재 완료!")
+def main():
+    query = input("어떤 강의를 수강하고 싶으신가요?: ")
+    env_vars = load_chroma_env_vars()
+    repo = ChromaRepository(env_vars["host"], env_vars["port"], 'lecture_reviews')
+    model = load_model()
+    return get_similar_reviews(query, repo, model)
+
+if __name__ == "__main__":
+    main()
+
