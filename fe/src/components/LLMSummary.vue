6636e461c4c8d21e73cd2cd21afe684efaec2df8
park_jun_hyoung <gitpdeathwa@yonsei.ac.kr>
2024-08-21 21:34:08 +0900

    ################ # 타입 리스트 # feat : 새로운 기능 추가 # fix : 버그 수정 # docs : 문서 수정 # test : 테스트 코드 추가 # refact : 코드 리팩토링 # style : 코드 의미에 영향을 주지 않는 변경사항 # chore : 빌드 부분 혹은 패키지 매니저 수정사항 ################ # "[타입] 제목" 의 형식으로 커밋 메시지를 아래 공백줄에 작성 # 변경사항이 "무엇"인지 명확히 작성 / 끝에 마침표 금지 # ex. [feat] 로그인 기능 추가 [feat] llm componenet 추가 # 바로 아래 공백은 지우지 마세요 (제목과 본문의 분리를 위함) [feat] loading 창 구현 임시... ################ # 본문(구체적인 내용)을 아랫줄에 성작성 # 생략 가능 # 여러 줄의 메시지를 작성할 땐 "-"로 구분
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (현재 커밋과 관련된 이슈 번호 추가 등)
    # 생략 가능
    # 예) Close #7

--- fe/src/components/LLMSummary.vue
+++ fe/src/components/LLMSummary.vue
@@ -6,17 +6,24 @@ const props = defineProps({
 })
 
 const summary = ref("")
-onMounted(async () => {
+const showSpinner = ref(false);
+onMounted(() => {
+  showSpinner.value = true;
   console.log("request llm lectureCode: " + props.lectureCode)
   summary.value = await fetch("http://localhost:8000/llm/" + props.lectureCode)
-      .then(response => response.json())
-      .then(resp => resp["summary"])
-      // .then(resp => resp["summary"].replace(/\n/g,'<br>'))
+      .then((response) => {
+        showSpinner.value = false;
+        response.json()["summary"];
+      }).catch(error => console.log(error))
 })
+
 
 </script>
 
 <template>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <div v-if="showSpinner === true"></div>
+  <div class="spinner-border" role="status"></div>
   <div id="llm-summary-container">
     <div style="white-space: pre-wrap;">
       {{summary}}
